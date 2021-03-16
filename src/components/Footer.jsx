819f6a5056a37256fce7557125e95c1fb9907b6c
Martin <martinroldan889@gmail.com>
2021-03-16 17:18:19 -0300

    Maquetado de home, creacion de Header y Footer y todos los estilos agregados

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import '../styles/components/Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="Footer">
+            <p className="Footer__title">Platzi Conf Merch</p>
+            <p className="Footer__copy">Todos los izquierdos reservados</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import '../styles/components/Header.css';
+
+const Header = () => {
+    return (
+        <header className="Header">
+            <h1 className="Header__title">Platzi Conf Merch</h1>
+            <div className="Header__checkout">
+                Checkout
+            </div>
+        </header>
+    )
+}
+
+export default Header
