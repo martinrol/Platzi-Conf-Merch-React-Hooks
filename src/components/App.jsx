3d964708ca67c858821920e9488ce638a6809bcc
Martin <martinroldan889@gmail.com>
2021-03-16 15:29:47 -0300

    Flujo de trabajo con ESLint y Prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 
 import '../styles/components/App.css';
 
-
-const App = () => (
-    <h1>Hola Mundo</h1>
-);
+const App = () => <h1>Hola Mundo</h1>;
 
 export default App;
