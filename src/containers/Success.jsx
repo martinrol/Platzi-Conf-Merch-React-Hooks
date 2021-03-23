c087029cccb896029151798ef16dd2c87f422f4d
Martin <martinroldan889@gmail.com>
2021-03-22 22:38:19 -0300

    Google Maps implementado, conectando con la API Key en su propio componente

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
+import AppContext from '../context/AppContext';
+import Map from '../components/Map';
 import '../styles/components/Success.css';
 
 const Success = () => {
+    const { state: { buyer } } = useContext(AppContext);
+
     return (
         <section className="Success">
             <div className="Success__content">
-                <h2>Martin, Thank you for your purchase</h2>
+                <h2>{`${buyer.name} Thank you for your purchase`}</h2>
                 <span>Your order will arrive in 3 days at your address: </span>
                 <div className="Success__content--map">
-                    Google Maps
+                    <Map />
                 </div>
             </div>
         </section>
